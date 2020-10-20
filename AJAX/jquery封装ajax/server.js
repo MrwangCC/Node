16634d663b0267a1a7c2e60895aa79458c5f0895
wangxiwen <15737531857@163.com>
2020-10-20 17:16:42 +0800

    jQuery封装ajax

---
+++ AJAX/jquery封装ajax/server.js
@@ -0,0 +1,31 @@
+let express = require('express');
+
+const app = express();
+// 用于解析post请求体参数，参数的编码类型必须为：urlencoded
+app.use(express.urlencoded({
+    extended: true
+}));
+
+// 暴露静态资源
+app.use(express.static(__dirname + '/public'));
+
+app.get('/ajax_get', (req, res) => {
+    console.log('有人发了个get请求');
+    console.log(req.query);
+    res.send('发来的是get请求');
+});
+app.post('/ajax_post', (req, res) => {
+    console.log('有人发了个post请求');
+    console.log(req.body);
+    res.send('发来的是post请求');
+});
+
+app.listen(3000, err => {
+    if (err) {
+        console.log('服务器启动失败：', err);
+    } else {
+        console.log('服务器启动成功');
+        console.log('练习jQuery发送ajax');
+        console.log('点击此处：http://localhost:3000/jquery_ajax.html');
+    }
+});
